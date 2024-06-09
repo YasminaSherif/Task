5ecb310ccdb7889f53b3380a5f91705df7f73ef0
yasmina sherif <yasminstyles77@gmail.com>
2024-06-09 17:50:02 +0300

    init ||||

--- PL/Pages/ClientsProducts/CreateClientProduct.cshtml.cs
+++ PL/Pages/ClientsProducts/CreateClientProduct.cshtml.cs
@@ -1,12 +1,43 @@
+using DTO.ClientProduct;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PL.Services;
 
 namespace PL.Pages.ClientsProducts
 {
     public class CreateClientProductModel : PageModel
     {
-        public void OnGet()
+        private readonly ClientProductService _clientProductService;
+
+        public CreateClientProductModel(ClientProductService clientProductService)
         {
+           _clientProductService = clientProductService;
         }
+
+        [BindProperty]
+        public ClientProductCreateDto clientProductDto { get; set; }
+
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            try
+            {
+                await _clientProductService.CreateClientProduct(clientProductDto);
+                return RedirectToPage("ClientsProudcts");
+            }
+            catch (Exception e)
+            {
+                ModelState.AddModelError(string.Empty, e.Message);
+
+                return Page();
+            }
+        }
+
     }
 }
+    
